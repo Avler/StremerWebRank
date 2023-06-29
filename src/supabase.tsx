70086dde992d8c1bb3149d201cb1a737650681f7
Kamil Sikora <avler455@gmail.com>
2023-06-30 00:15:20 +0200

    ver 04

--- src/supabase.tsx
+++ src/supabase.tsx
@@ -5,7 +5,7 @@ const supabaseKey =
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB1bWV6dmdwZWNwb3dsdmhoeXN4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODgwNjQxNzYsImV4cCI6MjAwMzY0MDE3Nn0.QEzLYvX-a1Ap5vtk4cJph7KORDqdxT9RB-ZF2kbiaw4';
 const supabase = createClient(supabaseUrl, supabaseKey);
 
-const channel = supabase.channel('test');
+const channel = supabase.channel('stremeres');
 
 channel
   .on('presence', { event: 'join' }, ({ key, newPresences }) => {
